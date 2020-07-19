edc037b8dc6deee462ea3324efd391a397cf81a1
Alexander Sadovskyi <alexandersadovskyi7@protonmail.com>
2020-07-19 13:03:31 +0300

    log actual difficulty of block

--- gateway/work.go
+++ gateway/work.go
@@ -40,13 +40,17 @@ func (b Block) NumberU64() uint64 { return b.number }
 
 var hasher = ethash.New()
 
-func (g *Gateway) submitBlock(submittedWork []string, blockNumber uint64, workerName string) {
+func (g *Gateway) submitBlock(submittedWork []string, blockNumber uint64, workerName string, actualTarget *big.Int) {
+	// Submitting the work first
+	status, err := g.parentWorkManager.Node.SubmitWork(submittedWork)
+
+	acutalShareDifficulty, _ := big.NewFloat(0).SetInt(big.NewInt(0).Div(utils.BigMax256bit, actualTarget)).Float64()
 	log.Logger.WithFields(logrus.Fields{
 		"nonce":        submittedWork[0],
 		"block-number": blockNumber,
 		"worker":       workerName,
+		"actual-diff":  humanize.SIWithDigits(acutalShareDifficulty, 2, "H"),
 	}).Info("⛏ Mined potential block")
-	status, err := g.parentWorkManager.Node.SubmitWork(submittedWork)
 	if err != nil {
 		log.Logger.WithFields(logrus.Fields{
 			"error": err,
@@ -94,15 +98,15 @@ func (g *Gateway) validateShare(submittedWork []string, workerName string) (type
 		blockValid, _ := hasher.Verify(block)
 
 		if blockValid {
-			g.submitBlock(submittedWork, blockNumber, workerName)
+			g.submitBlock(submittedWork, blockNumber, workerName, actualTarget)
 			// TODO add block to the DB
 			g.parentWorkManager.BestShareTarget = utils.BigMax256bit
 		} else {
 			if g.parentWorkManager.BestShareTarget.Cmp(actualTarget) == 1 {
 				float64ActualDifficulty, _ := big.NewFloat(0).SetInt(big.NewInt(0).Div(utils.BigMax256bit, actualTarget)).Float64()
 				log.Logger.WithFields(logrus.Fields{
-					"prefix":            "gateway",
-					"actual-difficulty": humanize.SIWithDigits(float64ActualDifficulty, 2, "H"),
+					"prefix":      "gateway",
+					"actual-diff": humanize.SIWithDigits(float64ActualDifficulty, 2, "H"),
 				}).Info("New best share")
 				g.parentWorkManager.BestShareTarget = actualTarget
 				// TODO add best share to db
