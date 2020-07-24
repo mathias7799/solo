6a642e0efc73591a3e8582cfc6bbb79d5288e142
Alexander Sadovskyi <43043344+AlexSSD7@users.noreply.github.com>
2020-07-24 10:56:11 +0300

    typo

--- gateway/workmanager.go
+++ gateway/workmanager.go
@@ -162,7 +162,7 @@ func NewWorkManager(bind string, shareDiff uint64, node *nodeapi.Node, engineWai
 			"prefix":       "workmanager",
 			"notification": workNotification,
 			"user-agent":   r.UserAgent(),
-		}).Debug("Received new work notification")
+		}).Debug("Received a new work notification")
 
 		if len(workNotification) != 4 {
 			log.Logger.WithFields(logrus.Fields{
